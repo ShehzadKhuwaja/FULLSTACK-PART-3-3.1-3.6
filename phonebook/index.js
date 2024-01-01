37d154d61d37c0ff7ebcf5fe349acdf4dd7a3ad4
ShehzadKhuwaja <shehzadkhuwaja@gmail.com>
2024-01-01 14:54:42 +0500

    3.2 completed

--- phonebook/index.js
+++ phonebook/index.js
@@ -2,29 +2,28 @@ const express = require('express')
 const app = express()
 
 let phonebookEntries = [
-    [
-        { 
-          "id": 1,
-          "name": "Arto Hellas", 
-          "number": "040-123456"
-        },
-        { 
-          "id": 2,
-          "name": "Ada Lovelace", 
-          "number": "39-44-5323523"
-        },
-        { 
-          "id": 3,
-          "name": "Dan Abramov", 
-          "number": "12-43-234345"
-        },
-        { 
-          "id": 4,
-          "name": "Mary Poppendieck", 
-          "number": "39-23-6423122"
-        }
-    ]
+    { 
+        "id": 1,
+        "name": "Arto Hellas", 
+        "number": "040-123456"
+    },
+    { 
+        "id": 2,
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523"
+    },
+    { 
+        "id": 3,
+        "name": "Dan Abramov", 
+        "number": "12-43-234345"
+    },
+    { 
+        "id": 4,
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122"
+    }
 ]
+
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -32,6 +31,10 @@ app.get('/', (request, response) => {
 
 app.get('/api/persons', (request, response) => {
     response.json(phonebookEntries)
+})
+
+app.get('/info', (request, response) => {
+    response.send(`<div><p>Phonebook has info for ${phonebookEntries.length} people</p><p>${new Date()}</p><div/>`)
 })
 
 const PORT = 3001
