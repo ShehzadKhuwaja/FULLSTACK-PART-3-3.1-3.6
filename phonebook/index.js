c52f4b4ed6b1ddb46708b4e8494819ba170c62c5
ShehzadKhuwaja <shehzadkhuwaja@gmail.com>
2024-01-02 20:39:25 +0500

    3.8 simplified

--- phonebook/index.js
+++ phonebook/index.js
@@ -25,21 +25,10 @@ let phonebookEntries = [
     }
 ]
 
+morgan.token('body', (req) => JSON.stringify(req.body))
 
 app.use(express.json())
-app.use(morgan((tokens, req, res) => {
-    return [
-        tokens.method(req, res),
-        tokens.url(req, res),
-        tokens.status(req, res, 'content-length'),
-        '-',
-        tokens['response-time'](req, res),
-        'ms',
-        JSON.stringify(req.body),
-    ].join(' ')
-}))
-
-morgan.token('body', (req) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
