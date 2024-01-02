d1a5a5b84951505a78ff58ba6c3dac390ff22523
ShehzadKhuwaja <shehzadkhuwaja@gmail.com>
2024-01-02 16:01:13 +0500

    3.4 completed

--- phonebook/index.js
+++ phonebook/index.js
@@ -48,6 +48,12 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+    phonebookEntries = phonebookEntries.filter(person => person.id !== Number(request.params.id))
+
+    response.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
